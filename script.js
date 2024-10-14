0d78ec79e03377abe227d214e9b1628bef036ce6
Richard <richardzou25@gmail.com>
2024-10-14 15:34:43 -0400

    saving pseudocode

---
+++ script.js
@@ -0,0 +1,11 @@
+// get container element from HTML where grid will go
+
+// create a function that creates a grid
+// needs to clear any existing content before creation
+// loop to create columns and rows
+// each square will be represented by a div
+
+// a hover effect that changes the grid color
+
+// a button that gets size for grid from user
+
