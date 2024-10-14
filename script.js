87c84579c99d4f8aee77a473a9e2c62f52b950c9
Richard <richardzou25@gmail.com>
2024-10-14 17:00:26 -0400

    add eventListener to button that called createGrid function

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 // get container element from HTML where grid will go
+const container = document.querySelector('.container')
+const button = document.querySelector('#generateNewGrid')
 
 // create a function that creates a grid
-// needs to clear any existing content before creation
-// loop to create columns and rows
-// each square will be represented by a div
+function createGrid(size) {
+    // needs to clear any existing content before creation
+    // loop to create columns and rows
+    for (let i = 0; i < size; i++) {
+
+    }
+    // each square will be represented by a div    
+}
+
 
 // a hover effect that changes the grid color
-
+button.addEventListener('click', function () {
+    let size = prompt("grid size: ")
+    createGrid(size)
+})
 // a button that gets size for grid from user
 
