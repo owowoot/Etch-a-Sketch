616b7a77a89ba350868d50daa9485cf204ada94d
Richard <richardzou25@gmail.com>
2024-10-18 06:20:17 -0400

    deleted RGB effect for standard color palette

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 // get container element from HTML where grid will go
 const container = document.querySelector('.container')
 const button = document.querySelector('#generateNewGrid')
+// color palette
+const colorPalette = ['red', 'blue', 'yellow', 'green', 'white', 'black'];
+let defaultColor = 'black';
+
+const paletteContainer = document.createElement('div');
+paletteContainer.classList.add('palette');
+// add element to top of the body
+document.body.insertBefore(paletteContainer, document.body.firstChild);
+for (let i = 0; i < colorPalette.length; i++) {
+    let colorButton = document.createElement('button');
+    colorButton.style.backgroundColor = [i];
+    colorButton.classList.add('color-option');
+    colorButton.setAttribute('data-color', colors[i]);
+
+    colorButton.addEventListener('click', function () {
+        defaultColor = this.getAttribute('data-color')
+    });
+
+    paletteContainer.appendChild(colorButton);
+}
 
 // Generate random RGB color
-function getRGBColor() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    return `rgb(${r}, ${g}, ${b})`;
-};
+// function getRGBColor() {
+//     const r = Math.floor(Math.random() * 256);
+//     const g = Math.floor(Math.random() * 256);
+//     const b = Math.floor(Math.random() * 256);
+//     return `rgb(${r}, ${g}, ${b})`;
+// };
 
 // create a function that creates a grid
 function createGrid(size) {
@@ -19,16 +39,24 @@ function createGrid(size) {
     // loop to create columns and rows
     for (let i = 0; i < size * size; i++) {
         // each square will be represented by a div
-        const div = document.createElement('div');
-        div.classList.add('grid-item')
+        let div = document.createElement('div');
+        div.classList.add('grid-item');
+        div.style.flexBasis = squareSize + 'px';
+        div.style.height = squareSize + 'px';
+        div.style.backgroundColor = 'white';
+        div.setAttribute('data-opacity', '0');
 
-        // Assign a random RGG color
-        const randomColor = getRGBColor();
-        div.style.backgroundColor = randomColor;
+        container.appendChild(div)
 
-        div.style.flexBasis = `${squareSize}px`;
-        div.style.height = `${squareSize}px`;
-        container.appendChild(div);
+        // // Assign a random RGG color
+        // const randomColor = getRGBColor();
+        // div.style.backgroundColor = randomColor;
+        // // initialize darkening
+        // div.setAttribute('data-darken', '0');
+
+        // div.style.flexBasis = `${squareSize}px`;
+        // div.style.height = `${squareSize}px`;
+        // container.appendChild(div);
     }
 
     container.style.display = 'flex';
@@ -55,7 +83,15 @@ function addHoverEffect() {
     for (let i = 0; i < squares.length; i++) {
         squares[i].addEventListener('mousemove', function () {
             if (isDrawing) {
-                this.style.backgroundColor = 'black';
+                let darkenColor = parseFloat(this.getAttribute('data-darken'));
+                if (darkenColor < 1) {
+                    darkenColor += 0.1;
+                    this.setAttribute('data-darken', darkenColor);
+
+                    let bgColor = this.style.backgroundColor;
+
+                    this.style.filter = `brightness(${1 - darkenColor})`;
+                }
             };
         });
     }
@@ -70,3 +106,4 @@ button.addEventListener('click', function () {
 })
 
 createGrid(16);
+addHoverEffect();
